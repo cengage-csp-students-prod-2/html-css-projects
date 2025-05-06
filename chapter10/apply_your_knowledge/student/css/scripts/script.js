bdd3cd00285443928e501a756d0d981430b1718e
MrLarry-94 <ep0143wp@go.minnstate.edu>
2025-05-06 04:33:01 +0000

    Testing Tue May 06 2025 04:33:01 GMT+0000 (Coordinated Universal Time)

---
+++ chapter10/apply_your_knowledge/student/css/scripts/script.js
@@ -0,0 +1,12 @@
+/*
+    Student Name: Lawrence Oladimeji
+    File Name: script.js
+    Date: 05/05/2025
+*/
+
+/* Function to display content */
+function content() {
+    var text = document.getElementById("new");
+    text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function.";
+    text.style.color = "#c0145f";
+    text.style.fontSize = "2em";
