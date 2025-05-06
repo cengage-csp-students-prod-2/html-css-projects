df38407ffd4c1a030cba577f3c9b88ba12173aac
MrLarry-94 <ep0143wp@go.minnstate.edu>
2025-05-06 05:04:17 +0000

    Testing Tue May 06 2025 05:04:17 GMT+0000 (Coordinated Universal Time)

--- chapter10/extend_your_knowledge/student/scripts/script.js
+++ chapter10/extend_your_knowledge/student/scripts/script.js
@@ -3,3 +3,19 @@
     File Name: script.js
     Date: 05/05/2025
 */
+
+// Function to hide paragraphs within the article
+function remove() {
+    $("#main p").hide();
+}
+
+// Function to show paragraphs within the article
+function display() {
+    $("#main p").show();
+}
+
+// Attach event handlers to the buttons once the DOM is loaded
+$(document).ready(function() {
+    $("#hideBtn").click(remove);
+    $("#showBtn").click(display);
+});
