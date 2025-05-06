a54ee54c90847a2d3e1140064390a521afa3ab94
MrLarry-94 <ep0143wp@go.minnstate.edu>
2025-05-06 06:35:16 +0000

    Testing Tue May 06 2025 06:35:16 GMT+0000 (Coordinated Universal Time)

--- chapter10/analyze_correct_improve/student/scripts/script.js
+++ chapter10/analyze_correct_improve/student/scripts/script.js
@@ -3,11 +3,21 @@
     File Name: script.js
     Date: 05/05/2025
 */
-
+var figElement = document.getElementById("placeholder");  
+var imgSource = document.getElementById("image");  
+var figCap = document.querySelector("figcaption");
 // Function to display the first picture
 function pic1() {
     imgSource.src = "images/trunk-bay.jpg";
     imgSource.alt = "Elevated view of Trunk Bay beach on St. John";
     figElement.style.display = "block";
     figCap.textContent = "Trunk Bay in St. John";
+}
+
+// Function to display the second picture
+function pic2() {
+    imgSource.src = "images/sanjuan.jpg";
+    imgSource.alt = "Elevated view of San Juan coast";
+    figElement.style.display = "block";
+    figCap.textContent = "Coast of San Juan";
 }
