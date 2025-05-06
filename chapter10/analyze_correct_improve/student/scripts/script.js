ca59c5e4d7a1f6fdedd62dee7fa9a1e39b9595bf
MrLarry-94 <ep0143wp@go.minnstate.edu>
2025-05-06 06:12:02 +0000

    Testing Tue May 06 2025 06:12:02 GMT+0000 (Coordinated Universal Time)

--- chapter10/analyze_correct_improve/student/scripts/script.js
+++ chapter10/analyze_correct_improve/student/scripts/script.js
@@ -4,10 +4,10 @@
     Date: 05/05/2025
 */
 
-//Function to display the first picture
-function pic1 {
-    imgSource.src = "images/trunk-bay.jpg"
-    imgSource.alt = Elevated view of Trunk Bay beach on St. John";
-    figElement.style.display = "block
+// Function to display the first picture
+function pic1() {
+    imgSource.src = "images/trunk-bay.jpg";
+    imgSource.alt = "Elevated view of Trunk Bay beach on St. John";
+    figElement.style.display = "block";
     figCap.textContent = "Trunk Bay in St. John";
 }
