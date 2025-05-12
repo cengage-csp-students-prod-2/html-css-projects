f24a792fff0b930b6f1867d6396f161caa01f282
MrLarry-94 <ep0143wp@go.minnstate.edu>
2025-05-12 23:25:42 +0000

    Testing Mon May 12 2025 23:25:42 GMT+0000 (Coordinated Universal Time)

---
+++ chapter10/ex02/student/rescue/scripts/script.js
@@ -0,0 +1,29 @@
+/*
+Author: Lawrence Oladimeji
+File Name: script.js
+Date: 05/12/2025
+*/
+
+// Function to answer question 1
+function ans1() {
+    document.getElementById("answer").innerHTML = 
+        "<p>Not all alone animals are orphans. Observe from a distance. If you don’t see a parent for several hours, call a licensed wildlife rehabilitator before intervening.</p>";
+}
+
+// Function to answer question 2
+function ans2() {
+    document.getElementById("answer").innerHTML = 
+        "<p>Rabid animals may show unusual aggression, confusion, excessive drooling, or paralysis. Keep your distance and contact animal control immediately.</p>";
+}
+
+// Function to answer question 3
+function ans3() {
+    document.getElementById("answer").innerHTML = 
+        "<p>No, birds have a limited sense of smell and will not reject their baby if touched by humans. Gently return the bird to its nest if safe.</p>";
+}
+
+// Function to answer question 4
+function ans4() {
+    document.getElementById("answer").innerHTML = 
+        "<p>You can volunteer by filling out the application form on our Contact Us page or emailing us at volunteer@wildrescues.net.</p>";
+}
