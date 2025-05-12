d7404ebc82b74ba241432a4c446b9283ac53b3ad
MrLarry-94 <ep0143wp@go.minnstate.edu>
2025-05-12 23:32:01 +0000

    Testing Mon May 12 2025 23:32:01 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex02/student/rescue/scripts/script.js
+++ chapter10/ex02/student/rescue/scripts/script.js
@@ -1,8 +1,25 @@
 /*
-Author: Lawrence Oladimeji
+Student Name: Lawrence Oladimeji
 File Name: script.js
 Date: 05/12/2025
 */
+
+//Global variables
+var answer = document.querySelector("#answer p");
+var heading = document.querySelector("#answer h2");
+
+//Hamburger menu function
+function menu() {
+    var navlinks = document.getElementById("nav-links");
+		var menuicon = document.getElementById("icon");
+		if (navlinks.style.display === "block") {
+		    navlinks.style.display = "none";
+				menuicon.style.color = "#2a1f14";
+		} else {
+		    navlinks.style.display = "block";
+				menuicon.style.color = "#f6eee4";
+		}
+}
 
 // Function to answer question 1
 function ans1() {
