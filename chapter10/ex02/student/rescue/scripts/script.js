35c6af06324eb33787f628d5a3508174d085d3c2
MrLarry-94 <ep0143wp@go.minnstate.edu>
2025-05-13 00:34:37 +0000

    Testing Tue May 13 2025 00:34:37 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex02/student/rescue/scripts/script.js
+++ chapter10/ex02/student/rescue/scripts/script.js
@@ -22,33 +22,21 @@ function menu() {
 }
 
 function ans1() {
-  const answerBox = document.querySelector("#answer p");
-  if (answerBox) {
-    answerBox.textContent =
-      "If the baby is warm, has a full belly, and is not crying, the mother is likely nearby. Observe from a distance before interfering.";
-  }
+  document.querySelector("#answer p").textContent =
+    "If the baby is warm, has a full belly, and is not crying, the mother is likely nearby. Observe from a distance before interfering.";
 }
 
 function ans2() {
-  const answerBox = document.querySelector("#answer p");
-  if (answerBox) {
-    answerBox.textContent =
-      "Common signs of rabies include excessive drooling, aggression, and strange behavior. Do not approach and call animal control.";
-  }
+  document.querySelector("#answer p").textContent =
+    "Common signs of rabies include excessive drooling, aggression, and strange behavior. Do not approach and call animal control.";
 }
 
 function ans3() {
-  const answerBox = document.querySelector("#answer p");
-  if (answerBox) {
-    answerBox.textContent =
-      "No, birds do not abandon their babies due to human scent. Gently return the baby to the nest if it is safe to do so.";
-  }
+  document.querySelector("#answer p").textContent =
+    "No, birds do not abandon their babies due to human scent. Gently return the baby to the nest if it is safe to do so.";
 }
 
 function ans4() {
-  const answerBox = document.querySelector("#answer p");
-  if (answerBox) {
-    answerBox.textContent =
-      "Visit our Contact Us page to fill out a volunteer form. We’ll be in touch with opportunities.";
-  }
+  document.querySelector("#answer p").textContent =
+    "Visit our Contact Us page to fill out a volunteer form. We’ll be in touch with opportunities.";
 }
